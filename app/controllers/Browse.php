eb2e400cb0e1284d62af8333a1d35b82cb33bf7b
Marcel Waterlander <112943380+o11899nine@users.noreply.github.com>
2024-02-19 22:58:20 +0100

    Fix cleanupbookdata function

--- app/controllers/Browse.php
+++ app/controllers/Browse.php
@@ -13,81 +13,45 @@ class Browse
     $bookModel = new BookModel;
     $books = $bookModel->findAll();
 
-    foreach ($books as $i => $book) {
-      $books[$i]["authors"] = $this->getAuthors($book);
-      $books[$i]["themes"] = $this->getThemes($book);
-    }
-
-
 
     $data["pageTitle"] = "Zoeken";
     $data["books"] = $this->cleanBookData($books);
     $data["filters"]["reading_level"] = $bookModel->findDistinct("reading_level", "ASC");
 
-
     $this->view('browse', $data);
   }
 
-  private function getAuthors(array $book)
-  {
 
-    $arr = [];
-
-    $bookAuthorModel = new BookAuthorModel;
-    $authorModel = new AuthorModel;
-
-    $authors = $bookAuthorModel->findWhere(["book_id" => $book["book_id"]]);
-
-    foreach ($authors as $author) {
-      $author = $authorModel->findFirst(["author_id" => $author["author_id"]]);
-      $authorName = ucfirst($author["first_name"]) . " " . strtolower($author["infix"]) . " " . ucfirst($author["last_name"]);
-      array_push($arr, $authorName);
-    }
-
-    return $arr;
-  }
-
-  private function getThemes(array $book)
-  {
-
-    $arr = [];
-
-    $bookThemeModel = new BookThemeModel;
-    $themeModel = new ThemeModel;
-
-    $themes = $bookThemeModel->findWhere(["book_id" => $book["book_id"]]);
-
-    if ($themes) {
-
-      foreach ($themes as $theme) {
-        $theme = $themeModel->findFirst(["theme_id" => $theme["theme_id"]]);
-        array_push($arr, $theme["theme"]);
-      }
-    }
-
-    return $arr;
-  }
   private function cleanBookData(array $books)
   {
     $cleanBookData = array();
 
     foreach ($books as $book) {
+      // Check if the book is already in the array
+      if (!isset($cleanBookData[$book["book_id"]])) {
 
-      $cleanBookData[$book["book_id"]] = array(
-        "book_id" => $book["book_id"],
-        "title" => trim(ucfirst($book["title"])),
-        "blurb" => mb_strimwidth(trim(ucfirst($book["blurb"])), 0, BLURBLENGTH, "..."),
-        "pages" => $book["pages"],
-        "year" => $book["publication_year"],
-        "reading_level" => $book["reading_level"],
-        "image_link" => !empty($book["image_link"]) ? $book["image_link"] : PLACEHOLDERCOVER,
-      );
-
-      foreach ($book["authors"] as $author) {
-        $cleanBookData[$book["book_id"]]["authors"][] = trim(str_replace("  ", " ", $author));
+        $cleanBookData[$book["book_id"]] = array(
+          "book_id" => $book["book_id"],
+          "title" => trim(ucfirst($book["title"])),
+          "blurb" => mb_strimwidth(trim(ucfirst($book["blurb"])), 0, BLURBLENGTH, "..."),
+          "pages" => $book["pages"],
+          "year" => $book["publication_year"],
+          "reading_level" => $book["reading_level"],
+          "image_link" => !empty($book["image_link"]) ? $book["image_link"] : PLACEHOLDERCOVER,
+          "authors" => array(),
+          "themes" => array()
+        );
       }
-      foreach ($book["themes"] as $theme) {
-        $cleanBookData[$book["book_id"]]["themes"][] = strtolower(trim($theme));
+			// Add the theme to the themes array for the current book
+      if (!in_array($book["theme"], $cleanBookData[$book["book_id"]]["themes"])) {
+        $cleanBookData[$book["book_id"]]["themes"][] = strtolower(trim($book["theme"]));
+      }
+
+      // Add the authors to the authors array for the current book
+      $author = ucfirst($book["first_name"]) . " " . strtolower($book["infix"]) . " " . ucfirst($book["last_name"]);
+      $author = trim(str_replace("  ", " ", $author));
+      if (!in_array($author, $cleanBookData[$book["book_id"]]["authors"])) {
+        $cleanBookData[$book["book_id"]]["authors"][] = $author;
       }
     }
 
--- app/models/BookModel.php
+++ app/models/BookModel.php
@@ -12,5 +12,73 @@ class BookModel
 
 	// 	'title',
 	// ];
+
+	public function findAll($order_column = "book_id", $order_type = "ASC")
+	{
+
+		$query = "SELECT
+      books.book_id,
+      books.title,
+      books.blurb,
+      books.pages,
+      books.reading_level,
+      books.image_link,
+      books.publication_year,
+      authors.first_name,
+      authors.infix,
+      authors.last_name,
+      themes.theme
+    FROM
+      books
+      LEFT JOIN books_authors ON books.book_id = books_authors.book_id
+      LEFT JOIN authors ON books_authors.author_id = authors.author_id
+      LEFT JOIN books_themes ON books.book_id = books_themes.book_id
+      LEFT JOIN themes ON books_themes.theme_id = themes.theme_id
+    ORDER BY
+			$order_column $order_type";
+
+		return $this->query($query);
+	}
+	
+	public function findWhere($data, $data_not = [], $order_column = "book_id", $order_type = "ASC")
+	{
+		$keys = array_keys($data);
+		$keys_not = array_keys($data_not);
+		$query = "SELECT
+      books.book_id,
+      books.title,
+      books.blurb,
+      books.pages,
+      books.reading_level,
+      books.image_link,
+      books.publication_year,
+      authors.first_name,
+      authors.infix,
+      authors.last_name,
+      themes.theme
+    FROM
+      books
+      LEFT JOIN books_authors ON books.book_id = books_authors.book_id
+      LEFT JOIN authors ON books_authors.author_id = authors.author_id
+      LEFT JOIN books_themes ON books.book_id = books_themes.book_id
+      LEFT JOIN themes ON books_themes.theme_id = themes.theme_id
+    WHERE ";
+
+
+		foreach ($keys as $key) {
+			$query .= $key . " = :" . $key . " && ";
+		}
+
+		foreach ($keys_not as $key) {
+			$query .= $key . " != :" . $key . " && ";
+		}
+
+		$query = trim($query, " && ");
+
+		$query .= " ORDER BY $order_column $order_type LIMIT $this->limit OFFSET $this->offset";
+		$data = array_merge($data, $data_not);
+
+		return $this->query($query, $data);
+	}
 	
 }
