8f82e2d3f557a11dfbd94c06afc0d47bc71cbcac
Marcel Waterlander <112943380+o11899nine@users.noreply.github.com>
2024-02-19 23:25:45 +0100

    change blurblength to 200

--- app/core/constants.php
+++ app/core/constants.php
@@ -2,5 +2,5 @@
 
 define('LOGO', ROOT . "/assets/img/sb-logo.png");
 define('PLACEHOLDERCOVER', ROOT . "/assets/img/placeholder-cover.png");
-define('BLURBLENGTH', 255);
+define('BLURBLENGTH', 200);
 define("MAXLIMIT", 9999999999);
