51c92b2e928e3284abb635d0b7de083839fe3f3d
Marcel Waterlander <112943380+o11899nine@users.noreply.github.com>
2024-02-22 07:15:32 +0100

    catch PDOException when inserting into books table

--- private/import.php
+++ private/import.php
@@ -8,34 +8,36 @@ require 'app/core/Database.php';
 
 //CLI command: php -r "require 'C:/xampp/htdocs/litlist/private/import.php';" 
 
-function run($filepath){
+function run($filepath)
+{
   $importModel = new Import;
   $importModel->csvToDb($filepath);
 }
 
-class Import {
-  
+class Import
+{
+
   use Database;
 
   public function csvToDb($filepath)
   {
-  
+
     $file = fopen("$filepath", "r");
     $length = 0; // 0 = maximum line length unlimited
     $separator = ";";
-  
+
     $cols = ["title", "image_link", "first_name", "last_name", "infix", "publication_year", "audiobook", "pages", "blurb", "summary", "genres", "themes", "reading_level", "recommendation_level", "review_text", "review_link", "secondary_literature_text", "secondary_literature_link"];
     $intCols = ["publication_year", "pages", "reading_level", "recommendation_level"];
     $strCols = ["image_link", "first_name", "last_name", "infix", "audiobook", "blurb", "summary", "genres", "themes", "review_text", "review_link", "secondary_literature_text", "secondary_literature_link"];
-  
-  
+
+
     $fileCols = fgetcsv($file, $length, $separator);
-  
+
     // Find row index for every col e.g. "title is the first column col", so $colIdx["title"] = 0
     foreach ($cols as $col) {
       $colIdx[$col] = array_search($col, $fileCols);
     }
-  
+
     $booksTotal = 0;
     $booksImported = 0;
     $booksFailed = 0;
@@ -44,57 +46,64 @@ class Import {
       $booksTotal += 1;
       // Create array with info for this book. E.g. $book["title] = 'Reis van flessen', $book["image_link"] 'http://www..." etc.
       $book = [];
-  
+
       foreach ($cols as $col) {
         $book[$col] = $bookRow[$colIdx[$col]];
       }
-  
+
       // If no title, stop and show which book
       if (empty($book["title"])) {
         $rowNum = $booksTotal + 1;
-        $this->show_error("BookNotFoundError", "row {$rowNum}");
+        $this->show_error("TitleNotFoundError", "row {$rowNum}");
         $booksFailed += 1;
         continue;
       }
-  
-  
-  
+      
+
+      
       $book = $this->setInvalidValuesToNull($book, $intCols, $strCols);
-  
+      
       // Clean themes
       $book["themes"] = $this->themesToArray($book["themes"]);
-  
-      // Escape special characters for SQL statement
+      
       $book = $this->trimArrayItems($book);
       $book = $this->escapeArrayItems($book);
-  
-  
-      // Data to go into 'books' table
-      $booksTableQuery = "INSERT INTO books (title, image_link, publication_year, audiobook, pages, blurb, summary, reading_level, review_text, review_link, secondary_literature_text, secondary_literature_link) 
-      VALUES (\"{$book['title']}\", \"{$book['image_link']}\", {$book['publication_year']}, \"{$book['audiobook']}\", {$book['pages']}, \"{$book['blurb']}\", \"{$book['summary']}\", {$book['reading_level']}, \"{$book['review_text']}\", \"{$book['review_link']}\", \"{$book['secondary_literature_text']}\", \"{$book['secondary_literature_link']}\");";
-  
-      // $result = $this->query($booksTableQuery);
-      // echo $result;
+
+      try {
+        $this->insertIntoBooksTable($book);
+      } catch (PDOException) {
+        $this->show_error("InsertError", $book["title"]);
+        $booksFailed += 1;
+      }
     }
-  
-  
+
+
     fclose($file);
-    echo "\n---\nREPORT\n---\n";
+    echo "---\nREPORT\n---\n";
     echo "BooksTotal: $booksTotal\n";
     echo "BooksFailed: $booksFailed\n";
     echo "BooksImported: $booksImported\n";
   }
-  
+
+  function insertIntoBooksTable($book)
+  {
+    // Data to go into 'books' table
+    $query = "INSERT INTO books (title, image_link, publication_year, audiobook, pages, blurb, summary, reading_level, review_text, review_link, secondary_literature_text, secondary_literature_link) 
+      VALUES (\"{$book['title']}\", \"{$book['image_link']}\", {$book['publication_year']}, \"{$book['audiobook']}\", {$book['pages']}, \"{$book['blurb']}\", \"{$book['summary']}\", {$book['reading_level']}, \"{$book['review_text']}\", \"{$book['review_link']}\", \"{$book['secondary_literature_text']}\", \"{$book['secondary_literature_link']}\");";
+
+    $this->query($query);
+  }
+
   function setInvalidValuesToNull($book, $intCols, $strCols)
   {
-  
+
     // Set empty values to NULL
     foreach ($book as $col => $value) {
       if ($book[$col] === "") {
         $book[$col] = "NULL";
       }
     }
-  
+
     // If INT expected, but not given -> set value to NULL
     foreach ($intCols as $col) {
       if (!filter_var($book[$col], FILTER_VALIDATE_INT) && $book[$col] !== "NULL") {
@@ -102,7 +111,7 @@ class Import {
         $book[$col] = "NULL";
       }
     }
-  
+
     // If STR expected, but INT given -> set value to NULL
     foreach ($strCols as $col) {
       if (filter_var($book[$col], FILTER_VALIDATE_INT) && $book[$col] !== "NULL") {
@@ -110,18 +119,18 @@ class Import {
         $book[$col] = "NULL";
       }
     }
-  
+
     return $book;
   }
-  
-  
+
+
   function escapeArrayItems(array $arr)
   {
     /* Escapes characters that will causes issues in SQL statement */
-  
+
     foreach ($arr as $key => $value) {
       if (isset($arr[$key])) {
-  
+
         if (is_array($arr[$key])) {
           $arr[$key] = $this->escapeArrayItems($arr[$key]);
         } else {
@@ -129,16 +138,16 @@ class Import {
         }
       }
     }
-  
+
     return $arr;
   }
-  
+
   function trimArrayItems(array $arr)
   {
-  
+
     foreach ($arr as $key => $value) {
       if (isset($arr[$key])) {
-  
+
         if (is_array($arr[$key])) {
           $arr[$key] = $this->trimArrayItems($arr[$key]);
         } else {
@@ -148,10 +157,10 @@ class Import {
         }
       }
     }
-  
+
     return $arr;
   }
-  
+
   function themesToArray($str)
   {
     $str = str_replace("\"", "", $str);
@@ -160,11 +169,11 @@ class Import {
     $themesArray = explode(",", $str);
     return $themesArray;
   }
-  
+
   function show_error($errorType, $book, $col = NULL, $oldValue = NULL, $newValue = NULL)
   {
     $msg = "\e[33m{$errorType} \e[0m";
-  
+
     if ($col) {
       $msg .= "{$col} ";
     }
@@ -174,7 +183,7 @@ class Import {
     if ($newValue) {
       $msg .= "\e[32m{$newValue} \e[0m";
     }
-  
+
     $msg .= "(\e[36m{$book}\e[0m)";
     print($msg . "\n");
   }
