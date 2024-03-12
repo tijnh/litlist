accbc786a3e879cee2dbe03c63b2679887859eb4
Marcel Waterlander <112943380+o11899nine@users.noreply.github.com>
2024-03-12 23:02:16 +0100

    typo

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -7,7 +7,7 @@
       </div>
       <div class="col-lg-6">
         <h1 class="display-5 fw-bold lh-1 mb-3">Welkom bij Litlist!</h1>
-        <p class="lead">Het Segbroek College presenteert: LitList! Op deze site vindt je alle boeken van de literatuurlijst van het Segbroek. Gebruik de zoekfilters om het boek te vinden dat bij jou past.</p>
+        <p class="lead">Het Segbroek College presenteert: LitList! Op deze site vind je alle boeken van de literatuurlijst van het Segbroek. Gebruik de zoekfilters om het boek te vinden dat bij jou past.</p>
         <div class="d-grid gap-2 d-md-flex justify-content-md-start">
           <a href="<?= ROOT ?>/browse" class="btn btn-primary btn-lg px-4 me-md-2">Zoek een boek</a>
           <button type="button" class="btn btn-outline-secondary btn-lg px-4" disabled>Login</button>
